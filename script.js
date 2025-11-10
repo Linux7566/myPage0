3ba8cfaece41b6e2e4873834564b0afbeb435c40
Asus <Linux7566>
2025-11-10 11:53:33 +0300

    update

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const addPhotoBtn = document.getElementById('addPhotoBtn');
-const photoArea = document.getElementById('photo-area');
 
-addPhotoBtn.addEventListener('click', () => {
-  const url = prompt('Введите URL фотографии');
-  if (url) {
-    const img = document.createElement('img');
-    img.src = url;
-    img.alt = 'Пользовательское фото';
-    photoArea.appendChild(img);
-  }
-});
